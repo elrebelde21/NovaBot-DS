7005c372dc3b136695c8a2dd2aca4fb7768352f3
natie1515 <frejulian57@gmail.com>
2025-11-22 12:58:46 -0400

    Refactor config-enable.js for improved readability

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -1,44 +1,47 @@
 let handler = async (message, { args, db, prefix, command }) => {
-const isEnable = /true|enable|(turn)?on|1/i.test(command);
-let settings = db.data.settings[message.guild.id] || {};
+    const isEnable = /true|enable|(turn)?on|1/i.test(command);
+
+    let settings = db.data.settings[message.guild?.id] || {};
     let chat = db.data.chats[message.channel.id] || {};
-    const user = db.data.users[message.author.id] || {};
+    let user = db.data.users[message.author.id] || {};
 
     const type = (args[0] || '').toLowerCase();
-    let isAll = false;
-    let isUser = false;
 
     switch (type) {
+
         case 'welcome':
             if (!message.guild) {
                 return message.reply("❌ Solo se puede usar en grupos.");
             }
             settings.welcome = isEnable;
+            db.data.settings[message.guild.id] = settings;
             break;
 
-        case 'antilink': case 'antiDiscord': 
+        case 'antilink':
+        case 'antidiscord':
             if (message.guild) {
                 chat.antiLink = isEnable;
             }
             break;
 
-case 'antilink2':
+        case 'antilink2':
             if (message.guild) {
-chat.antiLink2 = isEnable;
-}
-break
+                chat.antiLink2 = isEnable;
+            }
+            break;
 
         default:
             return message.reply(`
 Lista de opciones: welcome | antilink | antilink2
 
 Ejemplo:
-${prefix + command} welcome
-${prefix + command} welcome
+${prefix + command} welcome on
+${prefix + command} antilink off
 `.trim());
     }
 
-    db.data.settings[message.guild.id] = chat;
+    // Guardamos correctamente el chat
+    db.data.chats[message.channel.id] = chat;
     await db.write();
 
     message.reply(`
@@ -46,10 +49,13 @@ ${prefix + command} welcome
 `.trim());
 };
 
-handler.help = ['enable', 'disable'].map(v => v + ' <opción>');
+handler.help = ['enable <opción>', 'disable <opción>'];
 handler.tags = ['group'];
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i;
-handler.rowner = false
-handler.admin = false
-handler.botAdmin = false
+
+handler.register = false;
+handler.rowner = false;
+handler.admin = false;
+handler.botAdmin = false;
+
 export default handler;
