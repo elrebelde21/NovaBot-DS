4bc5549b9961eb5c2637809fecb23f7276df4d4a
natie1515 <frejulian57@gmail.com>
2025-12-15 18:12:49 -0400

    Refactor Tiktok API functions for improved safety

--- libs/tiktok.js
+++ libs/tiktok.js
@@ -12,47 +12,54 @@ async function shortener(url) {
 }
 
 export const Tiktok = async (query) => {
-  let response = await axios("https://lovetik.com/api/ajax/search", {
+  const response = await axios("https://lovetik.com/api/ajax/search", {
     method: "POST",
-    data: new URLSearchParams(Object.entries({ query })),
+    data: new URLSearchParams({ query }),
   });
 
-  result = {};
- 
+  const result = {};
+
   result.creator = "YNTKTS";
-  result.title = clean(response.data.desc);
-  result.author = clean(response.data.author);
+  result.title = clean(response.data?.desc || "");
+  result.author = clean(response.data?.author || "");
+
   result.nowm = await shortener(
-    (response.data.links[0].a || "").replace("https", "http")
+    (response.data?.links?.[0]?.a || "").replace("https", "http")
   );
+
   result.watermark = await shortener(
-    (response.data.links[1].a || "").replace("https", "http")
+    (response.data?.links?.[1]?.a || "").replace("https", "http")
   );
+
   result.audio = await shortener(
-    (response.data.links[2].a || "").replace("https", "http")
+    (response.data?.links?.[2]?.a || "").replace("https", "http")
   );
-  result.thumbnail = await shortener(response.data.cover);
+
+  result.thumbnail = await shortener(response.data?.cover || "");
+
   return result;
-}
+};
 
 async function ttimg(link) {
   try {
-        let url = `https://dlpanda.com/es?url=${link}&token=G7eRpMaa`;    
-        let response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
-        let imgSrc = [];
-        $('div.col-md-12 > img').each((index, element) => {
-            imgSrc.push($(element).attr('src'));
-        });
-        if (imgSrc.length === 0) {
-            return { data: '*[❗] No se encontraron imágenes en el enlace proporcionado.*' };
-        }
-        return { data: imgSrc }; 
-    } catch (error) {
-        console.lo (error);
-        return { data: '*[❗] No se obtuvo respuesta de la página, intente más tarde.*'};
-    };
-};
+    const url = `https://dlpanda.com/es?url=${link}&token=G7eRpMaa`;
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    let imgSrc = [];
+    $('div.col-md-12 > img').each((_, el) => {
+      imgSrc.push($(el).attr('src'));
+    });
+
+    if (!imgSrc.length) {
+      return { data: '*[❗] No se encontraron imágenes en el enlace proporcionado.*' };
+    }
+
+    return { data: imgSrc };
+  } catch (error) {
+    console.error(error);
+    return { data: '*[❗] No se obtuvo respuesta de la página, intente más tarde.*' };
+  }
+}
 
 export { ttimg };
