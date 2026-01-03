312760aece4d39066980e01cd1af63e3d6d1b84a
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2026-01-03 06:09:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-console.log('iniciando...')
-import { join, dirname } from 'path'
+console.log('iniciando...');
+import { join, dirname } from 'path';
 import { createRequire } from 'module';
 import { fileURLToPath } from 'url'
 import { setupMaster, fork } from 'cluster'
